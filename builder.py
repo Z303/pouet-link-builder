9474adacc82287d91d4b4cf991d441be8120b612
Zoe <github@z303.org>
2023-12-08 20:22:02 +0000

    Move name and group to functions

--- builder.py
+++ builder.py
@@ -2,6 +2,32 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 
+
+def process_name(soup):
+    # Extract the title
+    title = soup.title.string if soup.title else "No title found"
+    sections = title.split("by")
+    name = sections[0].strip()
+
+    list_of_all_links = soup.findAll('a')
+    youtube = ""
+
+    for link in list_of_all_links:                  
+        if "youtube" in link:
+            youtube = (link['href'])
+
+    return(f"<a href=\"{youtube}\">{name}</a>")
+
+
+def process_group(soup):
+    # Extract the title
+    title = soup.title.string if soup.title else "No title found"
+    sections = title.split("by")
+    group = sections[1].split("::")[0].strip()
+
+    return(f"{group}")  
+
+
 def process_line(input):
     POUET_PROD_TAG = "https://www.pouet.net/prod.php?which="
 
@@ -13,28 +39,17 @@ def process_line(input):
             if response.status_code == 200:
                 # Parse the HTML content
                 soup = BeautifulSoup(response.content, 'html.parser')
-                
-                # Extract the title
-                title = soup.title.string if soup.title else "No title found"
-                sections = title.split("by")
-
-                name = sections[0].strip()
-                group = sections[1].split("::")[0].strip()
 
                 demozoo_raw = str(soup.find("li", {"id": "demozooID"}))
                 demozoo_url = demozoo_raw.split("[")[1].split("]")[0]
- 
-                listOfAllLinks = soup.findAll('a')
-                youtube = ""
-
-                for link in listOfAllLinks:                  
-                    if "youtube" in link:
-                        youtube = (link['href'])
           
                 platform_raw = str(soup.find("span", {"class": "platform"}))
                 platform = platform_raw.split(">")[1].split("<")[0]
 
-                return(f"<a href=\"{youtube}\">{name}</a> by {group} for {platform} (<a href=\"{input}\">pouet</a>) ({demozoo_url})")
+                name = process_name(soup)
+                group = process_group(soup)
+
+                return(f"{name} by {group} for {platform} (<a href=\"{input}\">pouet</a>) ({demozoo_url})")
  
             else:
                 return("")
